d4ca50e571289d2f33b819edaed7b532e5d02ace
[AI 10]임도헌 <ldh2233@gmail.com>
2024-06-06 18:06:02 +0900

    New:client, server create

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express(); // express 객체 받아옴
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("헬로우 차징!");
+}); // HTTP GET method 정의
+
+app.listen("8000", () => {
+  console.log(`
+    #############################################
+        🛡️ Server listening on port: 8000 🛡️
+    #############################################    
+    `);
+}); // 8000번 포트에서 서버 실행
