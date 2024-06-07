9c99ab63da1fcd013c4c265e1f8c10f1ed9c1c32
[AI10] 원경혜 <wkh1204@gmail.com>
2024-06-07 14:43:17 +0900

    init: 프로젝트 초기 생성

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response, NextFunction } from 'express';
 
 const app = express(); // express 객체 받아옴
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("헬로우 차징!");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('헬로우 차징!');
 }); // HTTP GET method 정의
 
-app.listen("8000", () => {
+app.listen('8000', () => {
   console.log(`
     #############################################
         🛡️ Server listening on port: 8000 🛡️
