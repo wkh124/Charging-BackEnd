4c106997a93f4ff5248de0c49f5cb4cba81a1c45
[AI10] 원경혜 <wkh1204@gmail.com>
2024-06-21 22:59:59 +0900

    feature: 프로필 회원탈퇴 api 추가

--- server/src/routes/profileRouter.ts
+++ server/src/routes/profileRouter.ts
@@ -153,4 +153,25 @@ profileRouter.delete('/profile', ensureAuthenticated, async (req: Request, res: 
   }
 });
 
+
+// 회원 탈퇴
+profileRouter.get('/profile/logout', function (req, res, next) {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res
+          .clearCookie('connect.sid', {
+            secure: false,
+            httpOnly: true,
+            path: '/',
+          })
+          .status(200)
+          .send('Ok.');
+      }
+    });
+  }
+});
+
 export default profileRouter;
