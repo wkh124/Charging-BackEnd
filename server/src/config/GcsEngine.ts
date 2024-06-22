fd2961113f1e693be0fcdf5fa3e9b942e4604de6
Yoolim Han <ninetale22@gmail.com>
2024-06-22 13:19:30 +0900

    gcp 추가

---
+++ server/src/config/GcsEngine.ts
@@ -0,0 +1,93 @@
+import { Bucket, CreateWriteStreamOptions } from "@google-cloud/storage";
+import { Request } from "express";
+import multer from "multer";
+import path from "path";
+
+type nameFnType = (req: Request, file: Express.Multer.File) => string;
+type validatorFn = (req: Request) => string | null;
+
+type Options = {
+  bucket: Bucket;
+  options?: CreateWriteStreamOptions;
+  nameFn?: nameFnType;
+  validator?: validatorFn;
+};
+
+const defaultNameFn: nameFnType = (
+  _req: Request,
+  file: Express.Multer.File
+) => {
+  const fileExt = path.extname(file.originalname);
+
+  return `${file.fieldname}_${Date.now()}${fileExt}`;
+};
+
+export interface CustomFileResult extends Partial<Express.Multer.File> {
+  name: string;
+}
+
+class CustomStorageEngine implements multer.StorageEngine {
+  private bucket: Bucket;
+  private options?: CreateWriteStreamOptions;
+  private nameFn: nameFnType;
+  private validator?: validatorFn;
+
+  constructor(opts: Options) {
+    this.bucket = opts.bucket;
+    this.options = opts.options || undefined;
+    this.nameFn = opts.nameFn || defaultNameFn;
+    this.validator = opts.validator || undefined;
+  }
+
+  _handleFile = (
+    req: Request,
+    file: Express.Multer.File,
+    cb: (error?: any, info?: CustomFileResult) => void
+  ): void => {
+    if (!this.bucket) {
+      cb(new Error("bucket is a required field."));
+      return;
+    }
+
+    if (this.validator) {
+      // Validator is defined
+      const validationError = this.validator(req);
+
+      if (validationError) {
+        // If validator function returned a string which means there was an error
+        cb(new Error(validationError));
+        return;
+      }
+    }
+
+    const fileName = this.nameFn(req, file);
+
+    const storageFile = this.bucket.file(fileName);
+    const fileWriteStream = storageFile.createWriteStream(this.options);
+    const fileReadStream = file.stream;
+
+    fileReadStream
+      .pipe(fileWriteStream)
+      .on("error", (err) => {
+        fileWriteStream.end();
+        storageFile.delete({ ignoreNotFound: true });
+        cb(err);
+      })
+      .on("finish", () => {
+        cb(null, { name: fileName });
+      });
+  };
+
+  _removeFile = (
+    _req: Request,
+    file: Express.Multer.File & { name: string },
+    cb: (error: Error | null) => void
+  ): void => {
+    this.bucket.file(file.name).delete({ ignoreNotFound: true });
+    cb(null);
+  };
+}
+
+export default (opts: Options) => {
+  return new CustomStorageEngine(opts);
+};
--- server/src/routes/profilePicRouter.ts
+++ server/src/routes/profilePicRouter.ts
@@ -1,23 +1,84 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import { ensureAuthenticated } from '../middleware/authUser'; // Assuming you have this middleware
-import multerSetup from '../middleware/setup-multer'; // Assuming you have multer setup middleware
-import {url} from '../../config'; // Assuming db_connection is your database connection
 import  AuthenticatedRequest  from '../interfaces/authenticatedRequest';
 import { userDao } from '../DAO';
 
 const router = Router();
 
+// router.post("/profile-pics", ensureAuthenticated, multerSetup, async (req: Request, res: Response, next: NextFunction)  => {
+//     try {
+//     const { user } = req as AuthenticatedRequest;
 
-router.post("/profile-pics", ensureAuthenticated, multerSetup, async (req: Request, res: Response, next: NextFunction) => {
+//     if (!req.file) {
+//       throw res.status(400).json({
+//         error: '파일이 업로드되지 않았습니다.',
+//       });
+//     }
+
+//     if (!user) {
+//       return res.status(401).json({ error: '인증되지 않음' });
+//     }
+
+//     const profilePicUrl = `${url}/${req.file.path}`;
+
+//     const result =await userDao.userProfilePic(profilePicUrl, user.user_id); 
+
+//     if (result.length === 0) {
+//       return res.status(404).json({
+//         error: 'User not found',
+//       });
+//     }
+
+//     return res.status(200).json({
+//       success: true,
+//       image: req.file.path,
+//       fileName: req.file.filename,
+//     });
+
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
+
+
+// Connecting to google cloud storage
+import { Storage } from "@google-cloud/storage";
+import path from "path";
+
+const storage = new Storage({
+  projectId: "impactful-post-403213",
+  keyFilename: path.resolve(__dirname, "./", "service-account.json"),
+});
+
+
+// Selecting a bucket from storage
+const bucket = storage.bucket("gcpforelice");
+
+// Multer Config
+import multer from "multer";
+import GcsEngine, { CustomFileResult }  from "../config/GcsEngine";
+
+const nameFn = (_req: Request, file: Express.Multer.File) => {
+  const fileExt = path.extname(file.originalname);
+
+  // Do not forget to append file's extension
+  return `${file.originalname}~${new Date().getSeconds()}${fileExt}`;
+};
+
+const gcStorageWithoutValidator = GcsEngine({
+  bucket: bucket,
+  nameFn: nameFn,
+});
+
+const upload = multer({
+  storage: gcStorageWithoutValidator,
+}).single("profilePhoto");
+
+// Example route
+router.post("/profile-pics",ensureAuthenticated, upload, async(req: Request, res: Response, next:NextFunction) => {
   try {
     const { user } = req as AuthenticatedRequest;
-
-    if (req.fileValidationError) {
-      throw res.status(400).json({
-        error: req.fileValidationError,
-      });
-    }
-
+  
     if (!req.file) {
       throw res.status(400).json({
         error: '파일이 업로드되지 않았습니다.',
@@ -27,8 +88,10 @@ router.post("/profile-pics", ensureAuthenticated, multerSetup, async (req: Reque
     if (!user) {
       return res.status(401).json({ error: '인증되지 않음' });
     }
-
-    const profilePicUrl = `${url}/${req.file.path}`;
+    console.log(req.file);
+    const fileResult = req.file as CustomFileResult;
+    console.log(fileResult);
+    const profilePicUrl = `https://storage.cloud.google.com/gcpforelice/${(req.file as CustomFileResult).name}`;
 
     const result =await userDao.userProfilePic(profilePicUrl, user.user_id); 
 
@@ -49,5 +112,41 @@ router.post("/profile-pics", ensureAuthenticated, multerSetup, async (req: Reque
   }
 });
 
+// router.post("/profile-pics", ensureAuthenticated, multerSetup, async (req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     const { user } = req as AuthenticatedRequest;
+
+//     if (!req.file) {
+//       throw res.status(400).json({
+//         error: '파일이 업로드되지 않았습니다.',
+//       });
+//     }
+
+//     if (!user) {
+//       return res.status(401).json({ error: '인증되지 않음' });
+//     }
+
+//     const profilePicUrl = `${url}/${req.file.path}`;
+
+//     const result =await userDao.userProfilePic(profilePicUrl, user.user_id); 
+
+//     if (result.length === 0) {
+//       return res.status(404).json({
+//         error: 'User not found',
+//       });
+//     }
+
+//     return res.status(200).json({
+//       success: true,
+//       image: req.file.path,
+//       fileName: req.file.filename,
+//     });
+
+//   } catch (error) {
+//     return next(error);
+//   }
+// });
+
 export default router;
+
 
